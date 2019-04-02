2b57841de01e2a9620b445edf8a58ef02065e248
Danny <dannymanastireanu@gmail.com>
2019-04-02 22:01:27 +0300

    push

---
+++ NumberMemory/BetXmlParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NumberMemory {
+    class BetXmlParser {
+        private String filename;
+
+        public BetXmlParser(String filename) {
+            this.filename = filename;
+        }
+
+        public BetTableEntry[] readTableEntries() {
+            List<BetTableEntry> table = new List<BetTableEntry>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filename);
+            XmlNode tbody = doc.GetElementsByTagName("tbody")[0];
+            XmlNodeList rows = tbody.SelectNodes("tr");
+
+            foreach(XmlNode row in rows) {
+                XmlNodeList data = row.SelectNodes("td");
+                if(data.Count == 3) {
+                    int nonce = int.Parse(data[0].InnerText);
+                    int winnerNo = int.Parse(data[1].InnerText);
+                    int pot = int.Parse(data[2].InnerText);
+                    BetTableEntry temp = new BetTableEntry(nonce, winnerNo, pot);
+                    table.Add(temp);
+                } else {
+                    Console.Error.WriteLine("Invalid data length on row: " + row.InnerXml);
+                }
+
+            }
+
+            return table.ToArray();
+        }
+    }
+}
---
+++ NumberMemory/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace NumberMemory {
+    class Program {
+
+        private static string tableFilename = "bets.bin";
+
+        public static void Main(string[] args) {
+
+            if (args.Length == 1) {
+                if (File.Exists(args[0])) {
+
+                    BetTable betTable = null;
+
+                    if (File.Exists(tableFilename)) {
+                        FileStream fileStream = new FileStream(tableFilename, FileMode.Open, FileAccess.Read);
+                        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+                        betTable = (BetTable)binaryFormatter.Deserialize(fileStream);
+                        fileStream.Close();                        
+                    } else {
+                        betTable = new BetTable();
+                    }
+
+                    BetXmlParser parser = new BetXmlParser(args[0]);
+                    BetTableEntry[] entries = parser.readTableEntries();
+                   
+                    foreach (BetTableEntry var in entries) {
+                        betTable.add(var);
+                    }
+
+                    //Serialization
+                    {
+                        FileStream fileStream = new FileStream(tableFilename, FileMode.OpenOrCreate, FileAccess.Write);
+                        BinaryFormatter binaryFormatter = new BinaryFormatter();
+                        binaryFormatter.Serialize(fileStream, betTable);
+                        fileStream.Close();
+                    }
+
+                    File.Delete(args[0]);
+
+                } else {
+                    Console.Error.WriteLine("Invalid filename!");
+                    Environment.Exit(2);
+                }
+
+            } else {
+                Console.Error.WriteLine("Invalid arguments!");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
